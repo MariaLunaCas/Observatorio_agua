9781c3c02fe904a1173014247555a5c857566776
Julián Agudelo <114942366+JulianAgudelo7@users.noreply.github.com>
2026-04-25 23:58:54 +0000

    user crud terminado

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,4 +1,17 @@
 //estos son los unicos 2 metodos que se van a utilizar para insertar datos dentro de la base de datos
+import { IsString, MaxLength, IsEmail  } from "class-validator"
 
+export class CreateUserDto {
+  @IsString()
+  @MaxLength(25)
+  name: string;
 
-export class CreateUserDto {}
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  role: string;
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+//define las operaciones de CRUD
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor (
+    @InjectModel(User.name) private userModel: Model<User> 
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const createdUser = new this.userModel(createUserDto)
+    return createdUser.save(); 
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  async findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  async update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  async remove(id: string) {
     return `This action removes a #${id} user`;
   }
 }
